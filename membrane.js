8ca76ffa9a98e66d8da5c60aff334a524b0e6eb5
Gabriel Ferreira <gferreir@cs.cmu.edu>
2017-03-19 22:58:23 -0400

    simple tests added

--- membrane.js
+++ membrane.js
@@ -1,35 +1,44 @@
-'use strict';
-
 var membrane = {};
 var membraneContext = [];
 
-membrane.create = function(initTarget, bname) {
+membrane.functionCalls = new Map(); 
 
-  var wrap = function(target, bname) {
+membrane.create = function(initTarget, moduleName) {
+
+  var wrap = function(target, objectName) {
     if (Object(target) !== target) return target; // primitives are passed through
 
-	var handler = {
-		get: function(target, name, receiver) {
-			var t = Reflect.get(target, name, receiver);
-			if (Object(t) !== t) {
-				return t;
-			} else {
-		    	return wrap(t, bname+"."+name);
-			}
-	    },
-	    apply : function (target, thisArg, argumentsList) {
-	    	console.log("calling function: " + bname +"---" + target + " from context " + membraneContext.pop());
-	    	membraneContext.push(bname);
-	        var fCall = Reflect.apply(target, thisArg, argumentsList);
-	        membraneContext.pop();
-	        return fCall;
+  var handler = {
+    get: function(target, propertyName, receiver) {
+        var t = Reflect.get(target, propertyName, receiver);
+        if (Object(t) !== t) {
+          return t;
+        } 
+        return wrap(t, objectName + "." + propertyName);
+    },
+    apply : function (target, thisArg, argumentsList) {
+          var currentMembraneContext = membraneContext[membraneContext.length-1];
+          var currentContext = currentMembraneContext ? currentMembraneContext : "<rootModule>";
+
+          console.log("calling function: " + objectName + " from context " + currentContext);
+          membraneContext.push(objectName);
+
+          var counter = membrane.functionCalls.get(objectName);
+          counter = counter ? counter+1 : 1;
+          membrane.functionCalls.set(objectName, counter);
+
+          var fCall = Reflect.apply(target, thisArg, argumentsList);
+          membraneContext.pop();
+          return fCall;
         }
-	};
+  };
       
     var wrapper = new Proxy(target, handler);
     return wrapper;
   }
-  return wrap(initTarget, bname);
+  return wrap(initTarget, moduleName);
 }
 
 module.exports = membrane;
+
+
--- test/membrane-test.js
+++ test/membrane-test.js
@@ -1,77 +1,110 @@
 var assert = require('assert');
 var membrane = require('../membrane');
 
-describe('when creating a membrane around a function', function() {
-	it('should transparently return a wrapped function', function() {
-			var foo = function(){ return "foo"; };
-			var wrappedFunc = membrane.create(foo);
-			assert.equal(typeof wrappedFunc, 'function'); // types are kept the same
-			assert.notEqual(foo, wrappedFunc); // original instance and proxy are different objects
-			assert.equal(foo(), wrappedFunc()); // equal results
-		});
-});
+describe('Membrane Tests', function() {
 
+	var foo;
 
-describe('when creating a membrane around an object that contains a function', function() {
-		it('should transparently return a membrane wrapping the object and the internal function', function() {
-			var foo = function(){ return "foo"; };
-			var obj = { 'foo' : foo };
-
-			var wrappedObj = membrane.create(obj);
-			assert.equal(typeof wrappedObj, 'object'); // types are kept the same
-			assert.notEqual(obj, wrappedObj); // original instance and proxy are different objects
-			assert.notEqual(obj.foo, wrappedObj.foo); // original instance and proxy are different objects
-
-			assert.equal(obj.foo(), wrappedObj.foo()); // equal results
+	beforeEach(function() {
+		// initializers
+		membrane.functionCalls = new Map(); 
+	    foo = function(){ return "foo"; };
 	});
-});
 
-describe('when creating a membrane around a function', function() {
-	it('should transparently return a wrapped function', function() {
-		var foo = function(){ return "foo"; };
-		var fooMembrane = membrane.create(foo);
+	describe('when creating a membrane around a function', function() {
+		it('should transparently return a wrapped function', function() {
+				var wrappedFunc = membrane.create(foo, "foo");
+				assert.equal(typeof wrappedFunc, 'function'); // types are kept the same
+				assert.notEqual(foo, wrappedFunc); // original instance and proxy are different objects
+				assert.equal(foo(), wrappedFunc()); // equal results
 
-		assert.equal(typeof fooMembrane, 'function'); // types are kept the same
-		assert.notEqual(foo, fooMembrane); // original instance and proxy are different objects
-
-		assert.equal(foo(), fooMembrane()); // equal results
+				console.log(membrane.functionCalls);	
+			});
 	});
-});
 
-describe('when something', function() {
-	it('should something', function() {
-		var foo = function(){ 
-			var x=function(){return 4;}
-			x();
-			return "foo"; 
-		};
-		var bar = membrane.create({a:function() { return 3; }}, "bar")
-	});
-});
 
-describe('proxy around fs', function() {
-	it('should something', function() {
-		var fs = membrane.create(require("fs"),"fs")
+	describe('when creating a membrane around an object that contains a function', function() {
+			it('should transparently return a membrane wrapping the object and the internal function', function() {
+				var obj = { 'foo' : foo };
 
-		fs.readdir("/Users/gferreir/workspaces/jate", function(err, items) {
-			for (var i=0; i<items.length; i++) {
-				//console.log(" >>>" + items[i]);
-			}
+				var wrappedObj = membrane.create(obj, "foo");
+				assert.equal(typeof wrappedObj, 'object'); // types are kept the same
+				assert.notEqual(obj, wrappedObj); // original instance and proxy are different objects
+				assert.notEqual(obj.foo, wrappedObj.foo); // original instance and proxy are different objects
+
+				assert.equal(obj.foo(), wrappedObj.foo()); // equal results
+
+				console.log(membrane.functionCalls);	
 		});
 	});
-});
 
-describe('proxy around fs', function() {
-	it('should something', function() {
-		var foo = function(){return "foo";};
-		var bar = membrane.create({a:function() { return 3; }}, "bar");
-		var x = { 'foo' : foo };
-		var y = { 'x' : x };
-		var yMembrane = membrane.create(y,"y");
-		var dryX = yMembrane.x;
+	describe('when creating a membrane around a function', function() {
+		it('should transparently return a wrapped function', function() {
+			var fooMembrane = membrane.create(foo, "foo");
 
-		console.log(yMembrane.x.foo());
-		console.log(dryX.foo());
-		console.log(bar.a());
+			assert.equal(typeof fooMembrane, 'function'); // types are kept the same
+			assert.notEqual(foo, fooMembrane); // original instance and proxy are different objects
+
+			assert.equal(foo(), fooMembrane()); // equal results
+
+			console.log(membrane.functionCalls);			
+		});
 	});
+
+	describe('when something', function() {
+		it('should something', function() {
+			var foo = function(){ 
+				var x=function(){return 4;}
+				x();
+				return "foo"; 
+			};
+			var bar = membrane.create({a:function() { return 3; }}, "bar");
+			foo();
+			bar.a();
+
+			console.log(membrane.functionCalls);
+		});
+	});
+
+	describe('when creating a membrane around a module that require the fs module', function() {
+		it('should identify cross module function calls', function() {
+			var module = {};
+
+			module.execute = membrane.create(function() {
+				var fs = membrane.create(require("fs"), "fs");
+				fs.readdir("/Users/gferreir/workspaces/jate", function(err, items) {
+					for (var i=0; i<items.length; i++) {
+						//console.log("file: " + items[i]);
+					}
+				})
+			}, "testModule");
+
+			module.execute();
+			console.log(membrane.functionCalls);
+		});
+	});
+
+	describe('when wrapping an object, copying it to a dry object, and executing function foo in both', function() {
+		it('should count the function calls as being from the same wrapped object', function() {
+			var x = { 'foo' : foo };
+			var y = { 'x' : x };
+
+			var yMembrane = membrane.create(y, "y");
+			var dryX = yMembrane.x;
+
+			yMembrane.x.foo();
+			dryX.foo();
+
+			assert(membrane.functionCalls.get("y.x.foo"), 2);	
+		});
+	});
+
+	describe('when.. global object', function() {
+		it('should...', function() {
+			var g =  membrane.create(global, "global");
+			g.console.log("test");
+			
+		});
+	});
+
 });
